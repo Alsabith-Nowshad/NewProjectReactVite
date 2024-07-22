75195d49051b3d12a2ac4805659c9ca12ecd0221
Alsabith-Nowshad <alsabithnowshad123@gmail.com>
2024-07-22 17:24:51 +0530

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import './App.css'
+function Main(){
+  function click(){
+    alert("Order Places")
+  }
+
+
+  return (
+    <>
+    <div className="TopFirst">
+    <h1 className='TopText'>CHOOSE YOUR FAVORITE LOCATION FOR OFFERS, EVENTS, AND MORE!</h1>
+    </div>
+    <div className="SecTop">
+      <h2 className='SecText'>SHOP</h2>
+      <h2 className='SecText'>MENU</h2>
+      <h2 className='SecText'>CATERING</h2>
+      <h2 className='SecText'>LOCATION</h2>
+      <h2 className='SecText'>FRANCHISING</h2>
+      <h2 className='SecText'>ABOUT</h2>
+      <button onClick={click} className='SecBtn'>ORDER ONLINE</button>
+    </div>
+    <div className="TopImg">
+    <img className='ImageTop' src="img/61IHLDjNYoL._AC_UF1000,1000_QL80_.jpg" alt=""/>
+    </div>
+    <div className="BtmSec">
+      <h1 className='BtmFirst'>NEW DRINKS</h1>
+      <h3 className='BtmSecond'>A few of our summer favorites...</h3>
+    </div>
+    <div className="DrinksSec">
+      <div className="Drinks1">
+        <img className='img1' src="img/Fanta-PNG-Photo.png" alt="" />
+      </div>
+      <div className="Drinks2">
+        <img className='img2' src="img/pngtree-cocacola-can-resting-on-a-blank-png-image_10586760.png" alt="" />
+      </div>
+      <div className="Drinks3">
+        <img className='img3' src="img/pepsi-icon-0.png" alt="" />
+      </div>
+    </div>
+    <div className="DrinksText">
+      <div className="Drink1">
+      <h2 className='drnk'>NEW! VERY BERRY</h2>
+      <h2 className='DrinkSecText drnk'>FANTA ORANGE FLAVOURED</h2>
+      </div>
+      <div className="Drink2">
+      <h2 className='drnk'>NEW! VERY BERRY</h2>
+      <h2 className='DrinkSecText drnk'>FANTA ORANGE FLAVOURED</h2>
+      </div>
+      <div className="Drink3">
+      <h2 className='drnk'>NEW! VERY BERRY</h2>
+      <h2 className='DrinkSecText drnk'>FANTA ORANGE FLAVOURED</h2>
+      </div>
+    </div>
+    <div className="last">
+      <div className="ImgDiv">
+        <img className='LastImg' src="img/lastimg.jpg" alt="" />
+      </div>
+      <div className="ImgText">
+        <h4 className='lasttxt'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sint minima officia? Dicta vitae doloremque id voluptas modi quidem itaque quod ratione iste perspiciatis, sapiente aspernatur alias? Adipisci, illo quisquam?
+        Accusantium iure sequi enim, at eum sed blanditiis magni temporibus animi deleniti voluptatem numquam vitae impedit maxime! Saepe, dolorem corrupti soluta numquam, modi, quam reiciendis nobis unde animi officia sequi!
+        Quas nesciunt odio inventore ipsam quis, earum quos cum vitae corporis non beatae dolore, ullam debitis officia. Temporibus alias, numquam, ratione magnam provident, mollitia soluta aspernatur sit atque rerum laboriosam!
+        Corporis fugiat voluptatem iure placeat atque officiis blanditiis quod reprehenderit et tempore, odit nobis temporibus fuga eaque sapiente nemo! Sunt tempore enim velit natus, hic earum doloribus laborum doloremque vel.
+        Vero sed, excepturi delectus illo labore neque suscipit hic. Voluptatum officiis fugiat explicabo dolorum. Quaerat tempore, nobis perferendis, magnam voluptates voluptate atque quae officiis quisquam quidem laborum expedita fugit dicta.
+        Repellendus, possimus consectetur. Id, assumenda illum perspiciatis quae aspernatur veniam ad in tempore ipsa quos voluptatibus mollitia facere fuga consequatur, ullam suscipit molestiae. Similique incidunt blanditiis cum harum, eveniet quis.
+        Sapiente numquam, eligendi commodi totam odit alias aliquam facilis quam. Non dolor quidem vel reprehenderit, fugiat aliquid consectetur doloremque ad officia consequuntur molestiae aperiam corrupti odit in natus ea facere.
+        Nesciunt voluptate illum eius id rerum recusandae harum autem commodi est praesentium, error quis reprehenderit aliquid excepturi ratione similique architecto ullam veniam fugit molestias dolorem hic tempora? Minima, consequatur numquam!
+        Itaque doloribus, reiciendis, dolores repellendus excepturi aliquid iure hic beatae consequatur libero voluptatibus! Praesentium ab tenetur nobis molestias vitae error architecto reprehenderit, perferendis aut cumque mollitia possimus et recusandae illum?
+        Error ad quam voluptatibus doloremque unde incidunt tempora, ducimus esse deserunt repellendus, explicabo enim possimus illum consectetur natus aperiam! Facere sit obcaecati reiciendis labore autem tempora, beatae libero sint ipsum?
+        Illo vel ut, porro optio iste vitae quisquam aspernatur quam quo minus accusantium quos nesciunt quibusdam impedit consequatur unde vero consequuntur, delectus expedita eius odio cupiditate quidem doloremque dolorem! Unde?
+        Dignissimos eveniet numquam facere accusamus. Cumque quidem dolorem eligendi ex quae fuga vel animi eos deleniti corrupti tempore et cum illo, est nostrum optio a nam alias iure architecto voluptatum.
+        Soluta et ex assumenda, delectus repudiandae, tempore eveniet deleniti laudantium nostrum ad sint deserunt quibusdam aliquam laboriosam illum vitae odio velit, nisi voluptas? Fuga sapiente placeat architecto quibusdam, expedita atque.
+        </h4>
+      </div>
+    </div>
+    <div className="Footer">
+      
+      <div className="footText1">
+      <h1 className='lsttxtTop'>QUICK LINKS</h1>
+      <h1 className='lsttxt'>Beverage Menu</h1>
+      <h1 className='lsttxt'>Food Menu</h1>
+      <h1 className='lsttxt'>Catering</h1>
+      <h1 className='lsttxt'>Shop</h1>
+      <h1 className='lsttxt'>About</h1>
+      <h1 className='lsttxt'>Franchising</h1>
+      </div>
+      <div className="footText1 footText2">
+      <h1 className='lsttxtTop'>STAY CONNECTED</h1>
+      <h1 className='lsttxt'>Get the Mobile App</h1>
+      <h1 className='lsttxt'>Join Email List</h1>
+      <h1 className='lsttxt'>Loyalty Program</h1>
+      <h1 className='lsttxtTop1'>JOIN TEXT CLUB</h1>
+      <h1 className='lsttxt'>Join the Club</h1>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Main
